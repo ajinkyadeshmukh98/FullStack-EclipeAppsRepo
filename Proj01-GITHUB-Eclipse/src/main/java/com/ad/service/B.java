6d0db6fada0d41c056016f5c27f6af9b92d88b83
Ajinkya Deshmukh <ajinkyadeshmukh131@gmail.com>
2024-05-02 15:48:08 +0530

    Done some changes in sop message

--- Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
+++ Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
@@ -3,7 +3,8 @@ package com.ad.service;
 public class B {
    public static void main(String[] args) {
 	System.out.println("B.java file is made for making and reflecting some changes");
-
+   System.out.println("B.main()");
+   System.out.println("Made chnages in B.java");
 
 	
 }
