68e003b76cb414820796bded722765bca2fddfbd
Ajinkya Deshmukh <ajinkyadeshmukh131@gmail.com>
2024-05-02 16:03:09 +0530

    conflict

--- Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
+++ Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
@@ -4,6 +4,7 @@ public class B {
    public static void main(String[] args) {
 	   int x=20;
 	   int y=30;
+	   int z=50;
 	   
 	System.out.println("B.java file is made for making and reflecting some changes");
  
