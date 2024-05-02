f88f2d3aabe4d876b2a063fc11860b37079a4d26
Ajinkya Deshmukh <ajinkyadeshmukh131@gmail.com>
2024-05-02 15:54:19 +0530

    Conflict

--- Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
+++ Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
@@ -3,7 +3,7 @@ package com.ad.service;
 public class B {
    public static void main(String[] args) {
 	System.out.println("B.java file is made for making and reflecting some changes");
-   System.out.println("Made changes in B.java");
+ 
   
 
 	
