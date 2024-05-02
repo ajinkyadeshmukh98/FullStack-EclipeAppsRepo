d0a7a11aab0e060f0694909f8d1e4a3f8e8ab8e4
Ajinkya Deshmukh <ajinkyadeshmukh131@gmail.com>
2024-05-02 15:58:14 +0530

    int

--- Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
+++ Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
@@ -2,6 +2,9 @@ package com.ad.service;
 
 public class B {
    public static void main(String[] args) {
+	   int x=20;
+	   int y=30;
+	   
 	System.out.println("B.java file is made for making and reflecting some changes");
  
   
