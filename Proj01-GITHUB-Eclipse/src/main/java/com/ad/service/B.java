1d1b33048baf2bb8b826b6fd7b293b0f4b15c492
Ajinkya Deshmukh <ajinkyadeshmukh131@gmail.com>
2024-05-02 15:52:39 +0530

    Done some changes

--- Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
+++ Proj01-GITHUB-Eclipse/src/main/java/com/ad/service/B.java
@@ -3,8 +3,8 @@ package com.ad.service;
 public class B {
    public static void main(String[] args) {
 	System.out.println("B.java file is made for making and reflecting some changes");
-   System.out.println("B.main()");
    System.out.println("Made changes in B.java");
+   System.out.println("B.main()");
 
 	
 }
